b7b493aea1782a97257b6271fe3587765ae1f004
Álvaro Torres Cogollo <atorrescogollo@gmail.com>
2025-12-24 17:44:29 +0100

    Add sitemap and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://atorrescogollo.github.io',
+  site: "https://atorrescogollo.github.io",
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [
+    sitemap({
+      customPages: ["https://atorrescogollo.github.io/cv.pdf"],
+    }),
+  ],
 });
